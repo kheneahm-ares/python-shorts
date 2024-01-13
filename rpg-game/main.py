d7e6aac5f145fe6007a2db1a374d631c72d14b30
Kheneahm Ares <kheneahm9@gmail.com>
2024-01-13 12:27:43 -0600

    main class skeleton

--- rpg-game/main.py
+++ rpg-game/main.py
@@ -1,8 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 12 17:18:44 2024
+from configuration import *
+import pygame 
+import sys
 
-@author: kheneahm
-"""
+class Game:
+    def __init__(self):
+        self._screen = pygame.display.set_mode(MIN_WIDTH, MIN_HEIGHT)
+        self._clock = pygame.time.Clock()
+        self._running = True
+        
+    def createTileMap(self):
+        pass
+    
+    def create(self):
+        pass
+    
+    def update():
+        pass
+    
+    def events():
+        pass
 
+    def draw():
+        pass
+    
+    def main():
+        pass
+
+game = Game()
+game.create()
+
+while game.running:
+    game.main()
+    
+pygame.quit()
+sys.exit()
