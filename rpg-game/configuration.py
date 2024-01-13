d554cff351638d7d0893b164fd014d56d0cbf481
Kheneahm Ares <kheneahm9@gmail.com>
2024-01-13 13:21:06 -0600

    setting up sprites and layering

--- rpg-game/configuration.py
+++ rpg-game/configuration.py
@@ -1,2 +1,35 @@
 MIN_WIDTH = 800
 MIN_HEIGHT = 600
+TILE_SIZE = 32 #in pixels
+FPS = 60
+
+#layer order
+GROUND_LAYER=1
+BLOCKS_LAYER= 2
+
+#20x20 
+#B is block
+#. is empty area
+#each tile will 
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B...BBB............B',
+    'B..................B',
+    'B..................B',
+    'B.........BBB......B',
+    'B..........BB......B',
+    'B..................B',
+    'B......BBB.........B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'B......BBBB........B',
+    'B........BB........B',
+    'B.........B........B',
+    'B......BBBB........B',
+    'B..................B',
+    'B..........BBBBB...B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
--- rpg-game/main.py
+++ rpg-game/main.py
@@ -2,17 +2,29 @@ from configuration import *
 import pygame 
 import sys
 
+class Spritesheet:
+    def __init__(self, path):
+        self._spritesheet = pygame.image.load(file).convert()
+        
+    def game_image(self, x,y, width, height):
+        sprite = pygame.Surface([width, height])
+        sprite.blit(self._spritesheet, (0,0), (x,y,width,height))
+
+        return sprite
+    
+    
 class Game:
     def __init__(self):
         self._screen = pygame.display.set_mode(MIN_WIDTH, MIN_HEIGHT)
         self._clock = pygame.time.Clock()
+        self._terrain_spritesheet = Spritesheet('assets/images/terrain.png') #991,541
         self._running = True
         
     def createTileMap(self):
         pass
     
     def create(self):
-        pass
+        self._all_sprites = pygame.sprite.LayeredUpdates()
     
     def update():
         pass
