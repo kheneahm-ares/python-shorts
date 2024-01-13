7929af28c7c97332db34e633ec91017c674c6817
Kheneahm Ares <kheneahm9@gmail.com>
2024-01-13 14:14:21 -0600

    use base class

--- rpg-game/sprites.py
+++ rpg-game/sprites.py
@@ -1,8 +1,8 @@
 import pygame
 from configuration import *
 
-class Ground(pygame.sprite.Sprite):
-    def __init__(self, game, x,y):
+class BaseSprite(pygame.sprite.Sprite):
+    def __init__(self, game, x,y, layer, imageX, imageY):
         self._game = game
         
         #adding every block to groups
@@ -14,35 +14,20 @@ class Ground(pygame.sprite.Sprite):
         self._y = y*TILE_SIZE
         
         #inherited from sprite
-        self._layer = GROUND_LAYER
+        self._layer = layer
         self.width = TILE_SIZE
         self.height = TILE_SIZE
         
         #create block from blitted image
-        self.image = self._game._terrain_spritesheet.get_image(447, 353, self.width, self.height)
+        self.image = self._game._terrain_spritesheet.get_image(imageX, imageY, self.width, self.height)
         self.rect = self.image.get_rect() #set x, y coordinates of image
         self.rect.x = self._x
-        self.rect.y = self._y
+        self.rect.y = self._y 
 
-class Block(pygame.sprite.Sprite):
+class Ground(BaseSprite):
     def __init__(self, game, x,y):
-        self._game = game
-        
-        #adding every block to groups
-        self.groups = self._game._all_sprites
-        pygame.sprite.Sprite.__init__(self, self.groups)
-        
-        #every block will start at tile position * size
-        self._x = x*TILE_SIZE
-        self._y = y*TILE_SIZE
-        
-        #inherited from sprite
-        self._layer = BLOCKS_LAYER
-        self.width = TILE_SIZE
-        self.height = TILE_SIZE
-        
-        #create block from blitted image
-        self.image = self._game._terrain_spritesheet.get_image(991, 541, self.width, self.height)
-        self.rect = self.image.get_rect() #set x, y coordinates of image
-        self.rect.x = self._x
-        self.rect.y = self._y
+        super().__init__(game, x,y, GROUND_LAYER, 447, 353)
+
+class Block(BaseSprite):
+    def __init__(self, game, x,y):
+        super().__init__(game, x,y, BLOCKS_LAYER, 991, 541)
